81e8092ce110b5a616a1549ba73fda3057242069
kosuke876 <kangjiedi2@gmail.com>
2019-10-23 19:08:33 +0900

    修正点の改修

--- db/migrate/20191022101905_create_articles.rb
+++
@@ -1,8 +0,0 @@
-class CreateArticles < ActiveRecord::Migration[5.0]
-  def change
-    create_table :articles do |t|
-      t.string :name, null: false
-      t.timestamps null:false
-    end
-  end
-end
